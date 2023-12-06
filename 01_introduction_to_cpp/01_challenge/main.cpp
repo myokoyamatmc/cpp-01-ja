909d5b2415ec084e0bb3f27000bc9531e2acba6d
myokoyamatmc <masayuki_yokoyama_aa@mail.toyota.co.jp>
2023-12-06 22:31:09 +0900

    Submit challenge for grading

---
+++ 01_introduction_to_cpp/01_challenge/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int myNumber {100};
+    int anotherNumber {5};
+    
+    int answer = myNumber + anotherNumber;
+
+    std::cout << answer << std::endl;
+    return 0;
+}
